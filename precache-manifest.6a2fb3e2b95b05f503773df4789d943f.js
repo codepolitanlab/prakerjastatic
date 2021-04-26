feb5a2f575e08f2c7a971f90e415cf6da5b68fde
reinfajar <tsanimfajar@outlook.com>
2021-04-26 10:39:11 +0700

    init

---
+++ precache-manifest.6a2fb3e2b95b05f503773df4789d943f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b310bbe52130a9a796cc",
+    "url": "/css/about.ea8a899a.css"
+  },
+  {
+    "revision": "3b6eb69a20062bb34dfe",
+    "url": "/css/app.ad3a3bab.css"
+  },
+  {
+    "revision": "e34120e5e60064cf0bd0",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "1460dbd319db0f4f1494441cf3250eee",
+    "url": "/img/Bukalapak-1.1460dbd3.webp"
+  },
+  {
+    "revision": "b3ab3ddc1972e7cae22da981526bd268",
+    "url": "/img/Lebih_Cepat.b3ab3ddc.webp"
+  },
+  {
+    "revision": "4ca9548178a83bc001d38e5227a3709f",
+    "url": "/img/Logo-kartu-prakerja.4ca95481.webp"
+  },
+  {
+    "revision": "c9953c50aed833e03ec737378f4fe272",
+    "url": "/img/Study-Kasus.c9953c50.webp"
+  },
+  {
+    "revision": "f9fa2907a322de654cddc0b3f0baea48",
+    "url": "/img/Tentukan_Waktu_Sendiri.f9fa2907.webp"
+  },
+  {
+    "revision": "684add1f5c6fe384bedb95719359b843",
+    "url": "/img/img-mentoring.684add1f.png"
+  },
+  {
+    "revision": "4fafef4c93355504f47b710b8f25838a",
+    "url": "/img/kamu.4fafef4c.webp"
+  },
+  {
+    "revision": "4c2227cd8e8bcabdccb115b818513746",
+    "url": "/img/logo-cp.4c2227cd.webp"
+  },
+  {
+    "revision": "b32785a88822b6b636fdb8641df1bf6f",
+    "url": "/img/prakerja-about.b32785a8.webp"
+  },
+  {
+    "revision": "c0cc8a6b7cd77350c17f5666b9316777",
+    "url": "/img/prakerja.c0cc8a6b.webp"
+  },
+  {
+    "revision": "78fa78971693316b924f6b8065680b29",
+    "url": "/img/share.jpg"
+  },
+  {
+    "revision": "7ed54399ae2de9b416de56df02968f9e",
+    "url": "/img/tokopedia.7ed54399.webp"
+  },
+  {
+    "revision": "b4aacf9f5fb068c522e5d37c396fceeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b310bbe52130a9a796cc",
+    "url": "/js/about.488319a8.js"
+  },
+  {
+    "revision": "3b6eb69a20062bb34dfe",
+    "url": "/js/app.e3c55b08.js"
+  },
+  {
+    "revision": "e34120e5e60064cf0bd0",
+    "url": "/js/chunk-vendors.2f375202.js"
+  },
+  {
+    "revision": "2e7164384ed165c2be6218ab583ac5cc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
